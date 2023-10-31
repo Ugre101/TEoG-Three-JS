1c497ec04e46ce617d3bb58dc61a2584f95e1c2f
Ugre101 <teogadultgame@gmail.com>
2023-10-31 18:07:37 +0100

    made launch browser general

--- Börja Koda.bat
+++ Börja Koda.bat
@@ -4,4 +4,4 @@
 start cmd /k Powershell.exe -executionpolicy remotesigned -File "%~dp0RunViteCommand.ps1"
 
 :: Start the browser, if you are using a different browser than chrome, change the path to the executable
-start chrome /new-window http://localhost:5173/
+start "" http://localhost:5173/
