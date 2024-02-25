0cc6f50782c78833324cd3cd57f182063a0f6845
Ugre101 <44323564+Ugre101@users.noreply.github.com>
2024-02-25 23:46:58 +0100

    stuff

--- Scripts/Player.ts
+++ Scripts/Player.ts
@@ -1,6 +1,7 @@
 import {Race} from "./Character/RaceSystem";
 import {Character} from "./Character/Character";
 import { PerksDict } from "./Perk";
+import { VorePerksDict } from "./Vore/VorePerk";
 
 
 export class PlayerClass extends Character{
@@ -16,6 +17,12 @@ export function LoadPlayer(newPlayer: PlayerClass) {
     Player = newPlayer;
     Player.LevelSystem.perks.forEach((perk) => {
         let perkObj = PerksDict.get(perk);
+        if (perkObj) {
+            perkObj.onGain(Player);
+        }
+    });
+    Player.VoreSystem.vorePerks.forEach((perk) => {
+        let perkObj = VorePerksDict.get(perk);
         if (perkObj) {
             perkObj.onGain(Player);
         }
--- Scripts/Vore/VorePerk.ts
+++ Scripts/Vore/VorePerk.ts
@@ -5,19 +5,27 @@ import { Character } from "../Character/Character";
 export class VorePerk extends Perk{
     constructor(name: string, description: string, cost: number, type: string, 
         onGain: (char: Character) =>  void,requisites:Perk[] = [], exclusives:Perk[] = []) {
-        super(name, description, cost, type, onGain,requisites, exclusives);
+        super(name, description, cost, type, onGain);
     }
 }
 
-export const TierOneVorePerks = {
-    BeginnerPred: new VorePerk("Beginner Predator", "You're a beginner predator.", 1, "Vore", character => {
-        character.VoreSystem.voreStrengths.digestionStrength.mods.push(new Mod(10, "Beginner Predator", ModType.Percent));
-    }),
-    TinyPred: new VorePerk("Tiny Predator", "You're a tiny predator.", 1, "Vore", character => {
-    }),
-    GiantPred: new VorePerk("Giant Predator", "You're a giant predator.", 1, "Vore", character => {
-    }),
-};
-TierOneVorePerks.TinyPred.exclusives.push(TierOneVorePerks.GiantPred);
-TierOneVorePerks.GiantPred.exclusives.push(TierOneVorePerks.TinyPred);
+export const VorePerksDict = new Map<string, VorePerk>();
+
+const BeginnerPred = new VorePerk("Beginner Predator", "You're a beginner predator.", 1, "Vore", character => {
+    character.VoreSystem.voreStrengths.digestionStrength.mods.push(new Mod(10, "Beginner Predator", ModType.Percent));
+});
+VorePerksDict.set(BeginnerPred.stringId, BeginnerPred);
+
+const TinyPred = new VorePerk("Tiny Predator", "You're a tiny predator.", 1, "Vore", character => {
+    
+});
+VorePerksDict.set(TinyPred.stringId, TinyPred);
+
+const GiantPred = new VorePerk("Giant Predator", "You're a giant predator.", 1, "Vore", character => {
+    
+});
+VorePerksDict.set(GiantPred.stringId, GiantPred);
+
+TinyPred.exclusives.push(GiantPred);
+GiantPred.exclusives.push(TinyPred);
 
