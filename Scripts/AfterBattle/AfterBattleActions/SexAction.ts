95482dd2dba2978e34c32e2b81a11bf5a2474e17
Ugre101 <teogadultgame@gmail.com>
2024-05-10 11:31:07 +0200

    Update SexAction.ts

--- Scripts/AfterBattle/AfterBattleActions/SexAction.ts
+++ Scripts/AfterBattle/AfterBattleActions/SexAction.ts
@@ -2,9 +2,14 @@ import { Character } from "../../Character/Character";
 import { afterBattleManager } from "../AfterBattleManager";
 import { AfterAction, AfterActionType, ReqNeedDick } from "./AfterAction";
 
-
+enum SexRole {
+    Top,
+    Bottom,
+    Switch
+}
 class SexAction extends AfterAction {
-    constructor(name: string, description: string) {
+    public role: SexRole = SexRole.Switch;
+    constructor(name: string, description: string, role = SexRole.Switch) {
         super(name, description, AfterActionType.Sex);
     }
 }
@@ -16,7 +21,7 @@ Kiss.OnUse = (caster, target) => {
     return "You make out with your partner.";
 };
 
-const GetBlowJob = new SexAction("Get sucked", "Get a blowjob from your partner.");
+const GetBlowJob = new SexAction("Get sucked", "Get a blowjob from your partner.", SexRole.Top);
 GetBlowJob.reqs.push(ReqNeedDick.Caster);
 GetBlowJob.OnUse = (caster, target) => {
     return "";
@@ -25,7 +30,7 @@ GetBlowJob.OnContinueUse = (caster, target) => {
     return "";
 }
 
-const GiveBlowJob = new SexAction("Suck dick", "Give your partner a blowjob.")
+const GiveBlowJob = new SexAction("Suck dick", "Give your partner a blowjob.",SexRole.Bottom)
 GiveBlowJob.reqs.push(ReqNeedDick.Target);
 GiveBlowJob.OnUse = (caster,target) => {
     return "";
@@ -33,6 +38,41 @@ GiveBlowJob.OnUse = (caster,target) => {
 GiveBlowJob.OnContinueUse = (caster,target) => {
     return "";
 }
+
+const GetRimjob = new SexAction("Receive rimjob","", SexRole.Top);
+GetRimjob.OnUse = (caster, targer) => {
+    // If suffient dominant then facesitt?
+    return "";
+}
+GetRimjob.OnContinueUse = (caster, target) => {
+    return "";
+}
+
+const GiveRimjob = new SexAction("Give rimjob", "", SexRole.Bottom);
+GiveRimjob.OnUse = (caster, targer) => {
+    return "";
+}
+GiveRimjob.OnContinueUse = (caster, target) => {
+    return "";
+}
+
+const Missionary = new SexAction("Give misionary","", SexRole.Top);
+Missionary.OnUse = (caster, target) => {
+    return "";
+}
+Missionary.OnUse = (caster, target) => {
+    return "";
+}
+
+const ReceiveMissionary = new SexAction("Recive misionary","", SexRole.Bottom);
+ReceiveMissionary.OnUse = (caster, target) => {
+    return "";
+}
+ReceiveMissionary.OnContinueUse = (caster, target) => {
+    return "";
+}
+
+
 
 export const SexActions: SexAction[] = [
     Kiss, GetBlowJob, GiveBlowJob
