b4bd25b9db16e22f77c493a59c2060e792d17366
Ugre101 <teogadultgame@gmail.com>
2024-06-28 13:21:35 +0200

    Update MiscActions.ts

--- Scripts/AfterBattle/AfterBattleActions/MiscActions.ts
+++ Scripts/AfterBattle/AfterBattleActions/MiscActions.ts
@@ -1,3 +1,4 @@
+import { DormManagerInstance } from "../../Dorm/DormManager";
 import { afterBattleManager } from "../AfterBattleManager";
 import { AfterAction, AfterActionType } from "./AfterAction";
 
@@ -9,4 +10,17 @@ LeaveAction.OnUse = (caster, target) => {
     return "";
 }
 
+const TakeToDorm = new AfterAction("Take to Dorm", "Take the enemy to your dorm", AfterActionType.Misc);
+TakeToDorm.canUse = (caster, target) => {
+    if (target.SexStats.Orgasms >= 3)
+        return true;
+    return false;
+}
+TakeToDorm.OnUse = (caster, target) => {
+    console.log("Action used: " + TakeToDorm.name);
+    DormManagerInstance.addDormMate(target);
+    // TODO Check if there is more than one enemy
+    afterBattleManager.LeaveAfterBattle();
+    return "";
+}
 export const MiscActions = [ LeaveAction ];
